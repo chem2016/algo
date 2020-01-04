b4bb754e0cb25a9ebeaf33cde88522028974a4ba
Haoyu Yu <yuhaoyuapp@gmail.com>
2020-01-04 11:38:54 -0500

    word break I

---
+++ LintCode/DP/wordBreakI.py
@@ -0,0 +1,22 @@
+class Solution:
+    """
+    107. Word Break
+    中文English
+    Given a string s and a dictionary of words dict, determine if s can be broken into a space-separated sequence of one or more dictionary words.
+
+    Example
+    Example 1:
+        Input:  "lintcode", ["lint", "code"]
+        Output:  true
+
+    Example 2:
+	Input: "a", ["a"]
+	Output:  true
+    @param: s: A string
+    @param: dict: A dictionary of words dict
+    @return: A boolean
+    """
+    def wordBreak(self, s, dict):
+        # write your code here
+        # is possible -> or 
+        
