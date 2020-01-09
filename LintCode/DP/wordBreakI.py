5e29c88c391faee722fa1bc1f89b8b5449a70149
Haoyu Yu <yuhaoyuapp@gmail.com>
2020-01-08 21:55:13 -0500

    wordBreakI

--- LintCode/DP/wordBreakI.py
+++ LintCode/DP/wordBreakI.py
@@ -16,7 +16,26 @@ class Solution:
     @param: dict: A dictionary of words dict
     @return: A boolean
     """
-    def wordBreak(self, s, dict):
-        # write your code here
-        # is possible -> or 
-        
+    class Solution:
+    
+        def wordBreak(self, s, dict):
+            # write your code here
+            # abcdefgh
+            #   i-j  i
+            if len(dict) == 0:
+                return len(s) == 0
+            
+            n = len(s)
+            f = [False] * (n+1)
+            f[0] = True
+            maxLength = max([len(word) for word in dict])
+            
+            for i in range(1, n+1):
+                for j in range(1, min(maxLength, i)+1):
+                    if not f[i-j]:
+                        continue
+                    if s[i-j:i] in dict:
+                        f[i] = True
+                        break
+                    
+            return f[n]
